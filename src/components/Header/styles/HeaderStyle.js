89f3fc1c3411d62efc23934a1600f34fbe67d11f
Arslène <arskabinda@gmail.com>
2021-08-16 17:48:41 +0100

    test

--- src/components/Header/styles/HeaderStyle.js
+++ src/components/Header/styles/HeaderStyle.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const HeaderContainer = styled.div`
   background-color: #191919;
   display: flex;
-  flex-direction: row;
+
   justify-content: space-between;
   padding: 10px;
   padding-right: 50px;
